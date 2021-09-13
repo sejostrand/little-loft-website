cb2e184830b4126167f9a79d92e81ee5f915bbb0
Sebastian <sejostrand@gmail.com>
2021-09-13 17:28:17 +0100

    alignment corrections

--- components/MobilePhone.js
+++ components/MobilePhone.js
@@ -3,9 +3,9 @@ import styles from '../styles/MobilePhone.module.scss';
 
 const MobilePhone = ({ screenContentUrl }) => {
   return (
-    <div className={styles.mobilePhone_div}>
-      <img className={styles.mobileContent_img} src={screenContentUrl}></img>
-      <img className={styles.mobilePhone_img} src='./iphone_body.png'></img>
+    <div className={styles.mobilePhone__div}>
+      <img className={styles.mobileContent__img} src={screenContentUrl}></img>
+      <img className={styles.mobilePhone__img} src='./iphone_body.png'></img>
     </div>
   );
 };

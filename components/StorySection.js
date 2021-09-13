1ed8887ac411dcebdd47346a8b4ac267a426c213
Sebastian <sejostrand@gmail.com>
2021-09-13 22:54:53 +0100

    spacing ajustments

--- components/StorySection.js
+++ components/StorySection.js
@@ -4,7 +4,7 @@ import styles from '../styles/StorySection.module.scss';
 const StorySection = ({ content }) => {
   return (
     <section className={styles.story_section}>
-      <div className={styles.story_content_div}>
+      <div name='section-content' className={styles.story_content_div}>
         <h1 className={styles.story_h1}>{content.heading}</h1>
         <h2 className={styles.story_h2}>{content.subHeading}</h2>
         <p className={styles.story_p}>{content.paragraph}</p>
